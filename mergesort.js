5ee2d88eb96efc306a2851a2387d4ea3b66735eb
Eugene Monakoff <montemonakov@gmail.com>
2019-05-01 20:46:33 +0100

    simplified merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,46 +1,47 @@
 const numberGenerator = require('./number-generator');
 const numbers = numberGenerator(1, 100000);
-console.log(JSON.stringify(numbers));
 
-const sort = (numbers) => {
-  if (numbers.length < 2) return numbers;
-  const length = numbers.length;
-  const mid = Math.floor(length / 2);
+function merge (arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
 
-  const left = numbers.slice(0, mid);
-  const right = numbers.slice(mid);
+  while(i < arr1.length && j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+          results.push(arr1[i]);
+          i++;
+      } else {
+          results.push(arr2[j]);
+          j++;
+      }
+  }
 
-  return merge(sort(left), sort(right));
+  while(i < arr1.length) {
+      results.push(arr1[i]);
+      i++;
+  }
+
+  while(j < arr2.length) {
+      results.push(arr2[j]);
+      j++;
+  }
+
+  return results;
 };
 
-const merge = (left, right) => {
-  const result = [];
-
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      result.push(left.shift());
-    } else {
-      result.push(right.shift());
-    }
+function sort(arr) {
+  if (arr.length === 1) {
+      return arr;
   }
 
-  if (left.length) {
-    left.forEach(num => {
-      result.push(num);
-    });
-    left = [];
-  }
-
-  if (right.length) {
-    right.forEach(num => {
-      result.push(num);
-    });
-    right = [];
-  }
-
-  return result;
+  const mid = Math.round(arr.length/2);
+  return merge(
+      sort(arr.slice(0, mid)),
+      sort(arr.slice(mid))
+  );
 }
 
 console.time('Time this');
 const sortedNumbers = sort(numbers);
+console.log(sortedNumbers.slice(0, 10));
 console.timeEnd('Time this');
